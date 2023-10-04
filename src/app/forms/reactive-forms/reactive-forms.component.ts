a2fcefbea8fa5e4cacab6ae4aba2c9a965a873ac
abhi <pavadi.abhishek@gmail.com>
2023-10-04 18:35:57 +0530

    dynamicall add formcontrols

--- src/app/forms/reactive-forms/reactive-forms.component.ts
+++ src/app/forms/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup,FormArray, Validators } from '@angular/forms';
 import { PasswordValidator } from 'src/app/customvalidators/password.validator';
 import { nameValidator } from 'src/app/customvalidators/user-name.validator';
 
@@ -10,14 +10,61 @@ import { nameValidator } from 'src/app/customvalidators/user-name.validator';
 })
 
 export class ReactiveFormsComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  registrationForm:FormGroup | any;
+  ngOnInit() {
+    this.registrationForm=this.fb.group({
+      userName:['abhi',[Validators.required,Validators.minLength(3),nameValidator(/admin/)]], // admin username is not allowed
+      email:[''],
+      subscribe:[false],
+      password:[''],
+      confirmPassword:[''],
+      address:this.fb.group({
+        city:[''],
+        state:[''],
+        pincode:['']
+     }),
+     alternateEmails:this.fb.array([]),
+      
+  },{validator:PasswordValidator});
+  this.registrationForm.get('subscribe').valueChanges
+  .subscribe((checkedValue: any) =>{
+    const email=this.registrationForm.get('email');
+    if(checkedValue)
+    {
+      email.setValidators(Validators.required);
+    }else{
+      email.clearValidators;
+    }
+    email.updateValueAndValidity();
+  });
   }
   //----------------getuserName is used in Html for displaying validtion erors-------//
   get userName()
   {
     return this.registrationForm.get('userName');
   }
+  //-------------getEmail for eror validations in view page-------------//
+  get email()
+  {
+    return this.registrationForm.get('email');
+  }
+
+  //----------------get Alertnative emails for--------------- 
+  get alternateEmails()
+  {
+    return this.registrationForm.get('aleternateEmails') as FormArray;
+  }
+
+  //--- for adding dynamically add formcontrol------------//
+
+  addAlternateEmail()
+  {
+   
+    this.alternateEmails.push(this.fb.control(''));
+    
+
+  }
+
 constructor(private fb:FormBuilder){}
 // <!-----------------------------FormGroup-------------------->
   // registrationForm=new FormGroup(
@@ -51,17 +98,7 @@ constructor(private fb:FormBuilder){}
   // }
 
   //--------------------------------FORMBUILDER-------------------------//
-  registrationForm=this.fb.group({
-      userName:['abhi',[Validators.required,Validators.minLength(3),nameValidator(/admin/)]], // admin username is not allowed
-      password:[''],
-      confirmPassword:[''],
-      address:this.fb.group({
-        city:[''],
-        state:[''],
-        pincode:['']
-     })
-      
-  },{validator:PasswordValidator});
+
   displayValues()
   {
     this.registrationForm.patchValue(
